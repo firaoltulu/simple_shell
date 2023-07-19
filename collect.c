f7b03fe6fba2517726aa064f8ebc67ac27a84bd2
firaoltulu <firaoltulu5@gmail.com>
2023-07-19 10:30:02 -0700

    my sixth commit

--- collect.c
+++ collect.c
@@ -6,22 +6,22 @@
  */
 
 /*void free_all_dp(char **ptr)
-{
-    unsigned int i = 0;
+  {
+  unsigned int i = 0;
 
-    if (ptr == NULL)
-        return;
+  if (ptr == NULL)
+  return;
 
-    while (ptr[i])
-    {
-        free(ptr[i]);
-        i++;
-    }
+  while (ptr[i])
+  {
+  free(ptr[i]);
+  i++;
+  }
 
-    if (ptr[i] == NULL)
-        free(ptr[i]);
-    free(ptr);
-}*/
+  if (ptr[i] == NULL)
+  free(ptr[i]);
+  free(ptr);
+  }*/
 
 /**
  * parent_free_commands - free the buffer and the commands
@@ -32,25 +32,25 @@
 
 void parent_free_commands(char *buffer, char **ptr)
 {
-    free(buffer);
-    /*free_all_dp(commands);*/
-    int i = 0;
-    if (ptr == NULL)
-    {
-        return;
-    }
-    else
-    {
-        while (ptr[i])
-        {
-            free(ptr[i]);
-            i++;
-        }
+	free(buffer);
+	/*free_all_dp(commands);*/
+	int i = 0;
+	if (ptr == NULL)
+	{
+		return;
+	}
+	else
+	{
+		while (ptr[i])
+		{
+			free(ptr[i]);
+			i++;
+		}
 
-        if (ptr[i] == NULL)
-            free(ptr[i]);
-        free(ptr);
-    }
+		if (ptr[i] == NULL)
+			free(ptr[i]);
+		free(ptr);
+	}
 }
 
 /**
@@ -65,16 +65,16 @@ void parent_free_commands(char *buffer, char **ptr)
 
 void send_to_free(char *one, char **two)
 {
-    if (two == NULL)
-    {
-        parent_free_commands(one, two);
-    }
-    else if (_strcmp("exit", two[0]))
-    {
-        /**get_out(one, two);*/
-    }
-    else
-    {
-        parent_free_commands(one, two);
-    }
+	if (two == NULL)
+	{
+		parent_free_commands(one, two);
+	}
+	else if (_strcmp("exit", two[0]))
+	{
+		/**get_out(one, two);*/
+	}
+	else
+	{
+		parent_free_commands(one, two);
+	}
 }
